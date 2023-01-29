8ab4ab612234603460540d4f7a5a366b50a7abf0
Robert Beerta <beerta.robert@gmail.com>
2023-01-29 18:34:00 +0100

    add additional tests for rule 15

--- src/interfaces/AlertInterface.ts
+++ src/interfaces/AlertInterface.ts
@@ -1,6 +1,6 @@
-import { ActivationStatusEnum } from '../enums/AlertStatusEnum';
+import { AlertStatusEnum } from '../enums/AlertStatusEnum';
 
 export interface AlertInterface {
   id: string;
-  status: ActivationStatusEnum;
+  status: AlertStatusEnum;
 }
